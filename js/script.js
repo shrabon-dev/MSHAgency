93b19d6e6c25785c5473fb2ce80c9f504843e962
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2022-02-08 08:41:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,181 @@
+$(function () {
+
+    "use strict";
+
+    // =====Pre Loader===== //
+     
+    // var loader = document.getElementById("loader");
+    // window.addEventListener("load", function () {
+
+    //     loader.style.visibility = "hidden";
+
+    // });
+
+    // ======Menufix===== //
+
+       var navoff = $(".main_menu").offset().top;
+    
+       $(window).scroll(function () {
+           var scrolling = $(this).scrollTop();
+    
+           if (scrolling > navoff) {
+               $(".main_menu").addClass("menu_fix");
+           } else {
+               $(".main_menu").removeClass("menu_fix");
+           }
+       });
+
+
+     // =====Closes responsive menu when a scroll link is clicked===== //
+
+     $('.nav-link').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+
+    // ======Smooth Scroll===== //
+
+    // $('a[href*="#"]')
+    //     .not('[href="#"]')
+    //     .not('[href="#0"]')
+    //     .click(function (event) {
+    //         if (
+    //             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    //             location.hostname == this.hostname
+    //         ) {
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 event.preventDefault();
+    //                 $('html, body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000, function () {
+    //                     var $target = $(target);
+    //                     $target.focus();
+    //                     if ($target.is(":focus")) {
+    //                         return false;
+    //                     } else {
+    //                         $target.attr('tabindex', '-1');
+    //                         $target.focus();
+    //                     };
+    //                 });
+    //             }
+    //         }
+    //     });
+
+  
+    // ======Slick Slider===== //
+
+
+    $('.testi__details__slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000,
+        speed: 1300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    }
+  ]
+
+    });
+
+    
+    $('.featured_slider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000,
+        speed: 1300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    }
+  ]
+
+    });
+
+
+    // =====scroll top===== //
+
+
+    // $(".scroll_top").on('click', function () {
+    //     $("html,body").animate({
+    //         scrollTop: 0,
+    //     }, 800);
+    // });
+    // $(window).on('scroll', function () {
+    //     var scrolling = $(this).scrollTop();
+
+    //     if (scrolling > 200) {
+    //         $(".scroll_top").fadeIn();
+    //     } else {
+    //         $(".scroll_top").fadeOut();
+    //     }
+    // });
+
+
+
+    // =====Aos Js===== n//
+
+    AOS.init({
+        duration: 800,
+    });
+
+
+
+});
+
+
+$('#play-video').on('click', function(e){
+    e.preventDefault();
+    $('#video-overlay').addClass('open');
+    $("#video-overlay").append('<iframe width="360" height="115" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+  });
+  
+  $('.video-overlay, .video-overlay-close').on('click', function(e){
+    e.preventDefault();
+    close_video();
+  });
+  
+  $(document).keyup(function(e){
+    if(e.keyCode === 27) { close_video(); }
+  });
+  
+  function close_video() {
+    $('.video-overlay.open').removeClass('open').find('iframe').remove();
+  };
+
+  $(document).ready(function(){
+    $('.venobox').venobox(); 
+});
+
+
+
